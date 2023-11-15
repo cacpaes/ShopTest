61844cc602673df60d4c8a673b13fc2cc15c4bbe
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 11:04:10 +0000

    Add Attributes of the Sells class

---
+++ ShopTest/Entities/Sells.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ShopTest.Entities
+{
+
+    [Table("Sells")]
+    public class Sells
+    {
+        //Attributes of the Sells class
+
+        [Key]
+        public int SellsId { get; set; }
+        public int TransactionId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime SellDate { get; set; }
+    }
+}
