ce4b70262cd0b31750ef96326be6188ac0be4e8c
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 11:15:32 +0000

    Add Attributes of the Transactions class

---
+++ ShopTest/Entities/Transactions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ShopTest.Entities
+{
+    [Table("Transactions")]
+    public class Transactions
+    {
+        // Attributes of the Transactions class
+
+        [Key]
+        public int TransactionId { get; set; }
+        public int PersonId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string? TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
