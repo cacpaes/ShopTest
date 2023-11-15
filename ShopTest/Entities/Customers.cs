5ab14318994167b962a7f031e069f8a9aafc4275
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 10:50:17 +0000

    Additional attributes specific to Customers

---
+++ ShopTest/Entities/Customers.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ShopTest.Entities
+{
+    [Table("Customer")]
+    public class Customers : People
+    {
+        // Additional attributes specific to Customers
+        public decimal CreditLimit { get; set; }
+        public bool HasCard { get; set; }
+    }
+}
