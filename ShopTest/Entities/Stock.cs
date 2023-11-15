ddf424dbc51b0a6349d861f69f94e22b88b69018
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 11:09:20 +0000

    Add Attributes of the Stock class

---
+++ ShopTest/Entities/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ShopTest.Entities
+{
+    [Table("Stock")]
+    public class Stock
+    {
+        //Attributes of the Stock class
+
+        [Key]
+        public int StockId { get; set; }
+        public int ProductId { get; set; }
+        public int UnitQuantity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
+
