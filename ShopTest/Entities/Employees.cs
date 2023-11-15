c4358fe55248351a500f3b4aceb9aea6afbb9204
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 10:54:17 +0000

    Additional attributes specific to Employees

---
+++ ShopTest/Entities/Employees.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ShopTest.Entities
+{
+    [Table("Employee")]
+    public class Employees : People
+    {
+        //Additional attributes specific to Employees
+        public decimal Salary { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
