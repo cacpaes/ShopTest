17528c6a0c3708016bc22a0334c44f9cce947fb1
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 11:12:24 +0000

    Additional attributes specific to Suppliers

---
+++ ShopTest/Entities/Suppliers.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ShopTest.Entities
+{
+    [Table("Supplier")]
+    public class Suppliers : People
+    {
+        //Additional attributes specific to Suppliers
+
+        public string? ProductTypes { get; set; }
+        public int SupplierLevel { get; set; }
+        public bool Sells { get; set; }
+
+    }
+}
