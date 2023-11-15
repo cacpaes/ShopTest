1a4e0ccddb13e4364e2477a86919094fde09a596
cacarvalho.paes@hotmail.com <cacarvalho.paes@hotmail.com>
2023-11-15 11:24:07 +0000

    Add Attributes of the TypesReference class

---
+++ ShopTest/Entities/TypesReference.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ShopTest.Entities
+{
+    [Table("TypesReference")]
+    public class TypesReference
+    {
+        //Attributes of the TypesReference class
+
+        [Key]
+        //[Column("TypeId")] 
+        public int TypeId { get; set; }
+        public string? TypeSimbol { get; set; }
+        public string? TypeName { get; set; }
+        public string? TypeEntity { get; set; }
+        public string? TypeDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
+
